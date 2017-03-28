b6285201d251c4508399f3775e35d3227420622c
Garrett-Thomas <league@WTS10.attlocal.net>
2017-03-27 18:27:29 -0700

    /,lm

---
+++ src/introToGridWorld.java
@@ -0,0 +1,47 @@
+import info.gridworld.actor.Bug;
+import info.gridworld.actor.Flower;
+import info.gridworld.grid.Location;
+import info.gridworld.world.World;
+
+public class introToGridWorld {
+public static void main(String[] args) {
+	World sun = new World();
+ sun.show();
+Flower Dasiy = new Flower();
+Location pi = new Location(5,0); 
+sun.add(pi,Dasiy);
+Flower Dsiy = new Flower();
+Location li = new Location(4,0); 
+sun.add(li,Dsiy);
+Flower Dasy = new Flower();
+Location pil = new Location(3,0); 
+sun.add(pil,Dasy);
+Flower Dasiys = new Flower();
+Location pill = new Location(2,0);
+sun.add(pill,Dasiy);
+Flower Dasiyss = new Flower();
+Location pilll = new Location(1,0); 
+sun.add(pilll,Dasiyss);
+Flower Dasisy = new Flower();
+Location p = new Location(0,0); 
+sun.add(p,Dasisy);
+Flower spidr = new Flower();
+Location ni = new Location(6,1); 
+sun.add(ni,spidr);
+Flower Dasiysss = new Flower();
+Location pisss = new Location(5,1); 
+sun.add(pisss,Dasiysss);
+Flower Dsi = new Flower();
+Location lil = new Location(4,1); 
+sun.add(lil,Dsi);
+Flower Dassy = new Flower();
+Location piil = new Location(3,1); 
+sun.add(piil,Dasy);
+Flower Dasiiuys = new Flower();
+Location pilill = new Location(2,1);
+sun.add(pilill,Dasiiuys);
+Flower Dasiss = new Flower();
+Location plll = new Location(1,1); 
+sun.add(plll,Dasiss);
+}
+}

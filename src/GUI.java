78d2d78162d3413ed95bd29cbe299598514d6507
ColeTrammer <cole.trammer@gmail.com>
2017-04-03 18:27:28 -0700

    I LOVE SHADDING

---
+++ src/GUI.java
@@ -0,0 +1,10 @@
+import javax.swing.JFrame;
+
+public class GUI {
+	JFrame wind;
+void setup(){
+	wind = new JFrame();
+	wind.setVisible(true);
+wind.setSize(500,500);
+}
+}
---
+++ src/runner.java
@@ -0,0 +1,8 @@
+import java.nio.charset.MalformedInputException;
+
+public class runner {
+public static void main(String[] args) {
+	GUI win = new GUI();
+win.setup();
+}
+}

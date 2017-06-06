d3675d5b7b44d80022985ac585c0beee0baa5ba5
Ryguy-1 <league@WTS10.attlocal.net>
2017-06-05 18:27:37 -0700

    i did a lot today

---
+++ src/Rockrunn.java
@@ -0,0 +1,9 @@
+
+public class  Rockrunn {
+public static void main(String[] args){
+Squirel duck1 = new Squirel("orange");
+Squirel duck2 = new Squirel("orange");
+duck1.eat();
+
+}
+}
---
+++ src/Squirel.java
@@ -0,0 +1,13 @@
+
+public class Squirel {
+String myColor;
+public Squirel(String eat){
+myColor = eat;
+}
+void eat(){
+	if(myColor=="orange"){
+		System.out.println("orange");
+	}
+	
+}
+}

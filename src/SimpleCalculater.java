ebe9048a86b398c91f5528ed06cb17dad5dedd5f
alexthe31 <league@WTS10.attlocal.net>
2018-01-08 18:27:45 -0800

    This was an accident

--- src/SimpleCalculater.java
+++ src/SimpleCalculater.java
@@ -1,4 +1,13 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class SimpleCalculater {
-
+JFrame frame = new JFrame();
+JPanel pan = new JPanel();
+JButton butt1 = new JButton();
+JButton butt2 = new JButton();
+JButton butt3 = new JButton();
+JButton butt4 = new JButton();
+J
 }

14ec58eb8fa568a9e22b0e0f34b99433fd8b31dc
11000783 <League@AdministorsiMac.attlocal.net>
2018-02-05 18:25:51 -0800

    MakeTea, TurtlePond(IP)

---
+++ src/TeaMaker.java
@@ -0,0 +1,73 @@
+import javax.swing.JOptionPane;
+
+import Kettle.Water;
+
+// Copyright Wintriss Technical Schools 2013
+public class TeaMaker {
+	public static void main(String[] args) {
+		Kettle ket = new Kettle();
+		ket.boil();
+		TeaBag tea = new TeaBag("Green");
+		tea.getFlavor();
+		Cup cup = new Cup();
+		cup.makeTea(tea, ket.getWater());
+	}
+	/* Figure out how to make a cup of tea using the classes below */
+
+}
+
+class TeaBag {
+
+	public final static String GREEN = "Green";
+	public final static String MINT = "Mint";
+	public final static String CHAMOMILE = "Chamomile";
+	public final static String PASSION_FRUIT = "Passion Fruit";
+
+	private String flavor;
+
+	TeaBag(String flavor) {
+		this.flavor = flavor;
+	}
+
+	String getFlavor() {
+		return flavor;
+	}
+
+}
+
+class Kettle {
+
+	private Water water = new Water();
+
+	Water getWater() {
+		return water;
+	}
+
+	void boil() {
+		this.water.isHot = true;
+	}
+
+	class Water {
+
+		private boolean isHot = false;
+
+		public boolean isHot() {
+			return this.isHot;
+		}
+	}
+
+}
+
+class Cup {
+
+	private TeaBag teabag;
+
+	void makeTea(TeaBag teabag, Kettle.Water hotWater) {
+		this.teabag = teabag;
+		if (hotWater.isHot())
+			System.out.println("Making hot " + teabag.getFlavor() + " tea.");
+		else
+			System.out.println("Can't make tea with cold water! ");
+	}
+
+}
